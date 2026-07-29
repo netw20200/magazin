54a96ef8693b8af3a9aed350eb66e88abf58609c
Net Work <54335602+netw20200@users.noreply.github.com>
2026-07-29 13:04:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -79,8 +79,8 @@ function renderProducts(products) {
 
     return `
       <div class="card">
-        <img src="\( {img}" alt=" \){p.name}" loading="lazy" onerror="this.src='https://via.placeholder.com/300x300?text=Нет+фото'">
-        <div class="card-body">
+        <img src="${img}"
+           <div class="card-body">
           <div class="card-title">${p.name}</div>
           <div class="card-price">${price}</div>
           <a href="${tgLink}" class="btn" target="_blank">Заказать</a>
